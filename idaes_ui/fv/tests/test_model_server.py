1cc46cf251d4c78099742246496cd5cd70b4d361
Dan Gunter <dkgunter@lbl.gov>
2023-03-29 08:13:05 -0700

    Eliminate duplicate test

--- idaes_ui/fv/tests/test_model_server.py
+++ idaes_ui/fv/tests/test_model_server.py
@@ -63,27 +63,6 @@ def test_update_flowsheet(flash_model):
         srv.update_flowsheet("oscar")
 
 
-@pytest.fixture(scope="module")
-def flash_model():
-    """Flash unit model. Use '.fs' attribute to get the flowsheet."""
-    m = ConcreteModel()
-    m.fs = FlowsheetBlock(dynamic=False)
-    # Flash properties
-    m.fs.properties = BTXParameterBlock(
-        valid_phase=("Liq", "Vap"), activity_coeff_model="Ideal", state_vars="FTPz"
-    )
-    # Flash unit
-    m.fs.flash = Flash(property_package=m.fs.properties)
-    m.fs.flash.inlet.flow_mol.fix(1)
-    m.fs.flash.inlet.temperature.fix(368)
-    m.fs.flash.inlet.pressure.fix(101325)
-    m.fs.flash.inlet.mole_frac_comp[0, "benzene"].fix(0.5)
-    m.fs.flash.inlet.mole_frac_comp[0, "toluene"].fix(0.5)
-    m.fs.flash.heat_duty.fix(0)
-    m.fs.flash.deltaP.fix(0)
-    return m
-
-
 @pytest.mark.integration
 def test_flowsheet_server_run(flash_model):
     import requests
