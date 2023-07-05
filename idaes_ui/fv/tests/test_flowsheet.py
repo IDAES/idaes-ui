b8fd954ee9e416680f8e99c52cb2c3fca39d0703
CopyDemon <shengdevelop@gmail.com>
2023-07-05 10:46:18 -0700

    testing, instead of assert json.dumps, just assert boolean

--- idaes_ui/fv/tests/test_flowsheet.py
+++ idaes_ui/fv/tests/test_flowsheet.py
@@ -297,9 +297,9 @@ def test_flowsheet_serializer_demo(demo_flowsheet, demo_flowsheet_json):
     stored_dict = json.loads(demo_flowsheet_json)
     _canonicalize(test_dict)
     _canonicalize(stored_dict)
-    # assert json.dumps(test_dict, sort_keys=True) == json.dumps(stored_dict, sort_keys=True)
-    assert json.dumps(test_dict) == json.dumps(stored_dict)
-
+    isJSONEqual = False if json.dumps(test_dict, sort_keys=True) != json.dumps(stored_dict, sort_keys=True) else True
+    print(isJSONEqual)
+    assert isJSONEqual
 
 @pytest.mark.skipif(not helmholtz_available(), reason="General Helmholtz not available")
 @pytest.mark.component
