a2b3b66455af4442b6bdb95d2f771e420ea66f3e
CopyDemon <shengdevelop@gmail.com>
2023-07-07 09:40:56 -0700

    working on header create basic header and insert header items

--- IDAES-UI/src/components/header_component/header.tsx
+++ IDAES-UI/src/components/header_component/header.tsx
@@ -1,5 +1,13 @@
+import HeaderLogo from "./header_logo_component/header_logo";
+import HeaderFlowsheetName from "./header_flowsheet_name_component/header_flowsheet_name";
+import HeaderFunctionButtonsWrapper from "./header_function_buttons_wrapper_component/hader_function_buttons_wrapper";
+
 export default function Header(){
   return (
-    <div>header is here</div>
+    <header className={`holder`}>
+      <HeaderLogo />
+      <HeaderFlowsheetName />
+      <HeaderFunctionButtonsWrapper />
+    </header>
   )
 }
