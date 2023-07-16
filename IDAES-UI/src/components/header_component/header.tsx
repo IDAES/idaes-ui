76e0f8eef00d9de5548514f06e38dd83b0518ae2
CopyDemon <shengdevelop@gmail.com>
2023-07-16 12:10:04 -0700

    add context and header use context

--- IDAES-UI/src/components/header_component/header.tsx
+++ IDAES-UI/src/components/header_component/header.tsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+import { AppContext } from "../../context/appMainContext";
+
 import HeaderLogo from "./header_logo_component/header_logo";
 import HeaderFlowsheetName from "./header_flowsheet_name_component/header_flowsheet_name";
-import HeaderFunctionButtonsWrapper from "./header_function_buttons_wrapper_component/hader_function_buttons_wrapper";
+import HeaderFunctionButtonsWrapper from "./header_fn_btn_wrapper_component/hader_fn_btn_wrapper";
+
+import css from "./header.module.css";
 
 export default function Header(){
+  let val = useContext(AppContext)
+  console.log(val)
+  
   return (
-    <header className={`holder`}>
+    <header className={`row ${css.header_container}`}>
       <HeaderLogo />
       <HeaderFlowsheetName />
       <HeaderFunctionButtonsWrapper />
