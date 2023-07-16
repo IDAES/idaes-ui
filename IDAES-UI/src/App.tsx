acdfaae9a0730bc1e6a742a671f9fe2e33e01cf8
CopyDemon <shengdevelop@gmail.com>
2023-07-16 13:46:10 -0700

    ignore this please, this is only for testing joint js hack way is working

--- IDAES-UI/src/App.tsx
+++ IDAES-UI/src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { AppContext } from "./context/appMainContext";
 
 import { PanelGroup, Panel, PanelResizeHandle} from "react-resizable-panels";
@@ -14,10 +14,52 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+declare var joint: any;
+declare var paper: any;
 
 function App() {
   let context = useContext(AppContext);
-  const isShowTable = context.panelState[2].show;
+  // const isShowTable = context.panelState[2].show;
+
+  const canvas:any = useRef(null)
+
+  var namespace = joint.shapes;
+
+        var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+
+        var paper = new joint.dia.Paper({
+            el: document.getElementById('myholder'),
+            model: graph,
+            width: 600,
+            height: 100,
+            gridSize: 1,
+            cellViewNamespace: namespace
+        });
+
+        var rect = new joint.shapes.standard.Rectangle();
+        rect.position(100, 30);
+        rect.resize(100, 40);
+        rect.attr({
+            body: {
+                fill: 'blue'
+            },
+            label: {
+                text: 'Hello',
+                fill: 'white'
+            }
+        });
+        rect.addTo(graph);
+
+        var rect2 = rect.clone();
+        rect2.translate(300, 0);
+        rect2.attr('label/text', 'World!');
+        rect2.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect);
+        link.target(rect2);
+        link.addTo(graph);
+
   return (
         <>
           <Header/>
@@ -30,6 +72,9 @@ function App() {
               {context.panelState[2].show && <Table_row />}
             </Panel>
           </PanelGroup>
+
+          <div id="myholder"></div>
+          <div className="canvas" ref={canvas}></div>
         </>
   )
 }
