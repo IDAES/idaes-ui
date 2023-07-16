6e34ede10ac9690361150365280cbbfc7c9dd3e9
CopyDemon <shengdevelop@gmail.com>
2023-07-16 12:04:05 -0700

    create basic structure of app

--- IDAES-UI/src/App.tsx
+++ IDAES-UI/src/App.tsx
@@ -1,15 +1,36 @@
-import './App.css'
+import { useContext } from "react";
+import { AppContext } from "./context/appMainContext";
+
+import { PanelGroup, Panel, PanelResizeHandle} from "react-resizable-panels";
+
+
 
 import Header from "./components/header_component/header";
-import Flowsheet from './components/flowsheet_component/flowsheet';
+import FlowsheetWrapper from './components/flowsheet_main_component/flowsheet_wrapper';
 import Table_row from './components/table_row_component/table_row';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 function App() {
+  let context = useContext(AppContext);
+  const isShowTable = context.panelState[2].show;
   return (
-    <>
-      <Header />
-      <Flowsheet />
-      <Table_row />
-    </>
+        <>
+          <Header/>
+          <PanelGroup direction="vertical" style={{height : "80vh"}}>
+            <Panel maxSize={90}>
+              <FlowsheetWrapper />
+            </Panel>
+            <PanelResizeHandle style={{width: "10px", height:"5px", backgroundColor: "black", marginLeft : "20px", marginRight: "20px"}}/>
+            <Panel maxSize={90}>
+              {context.panelState[2].show && <Table_row />}
+            </Panel>
+          </PanelGroup>
+        </>
   )
 }
 
