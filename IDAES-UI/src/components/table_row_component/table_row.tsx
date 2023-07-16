240cbbe9ffe1b0f11a6b802c03ecdbe633502148
CopyDemon <shengdevelop@gmail.com>
2023-07-16 12:14:55 -0700

    create table compoent

--- IDAES-UI/src/components/table_row_component/table_row.tsx
+++ IDAES-UI/src/components/table_row_component/table_row.tsx
@@ -1,5 +1,26 @@
+import { useContext } from "react";
+import {AppContext} from "../../context/appMainContext";
+
 export default function TableRow() {
+  const context = useContext(AppContext);
+  const isShowTable = context.panelState[2].show
   return(
-    <div>table row</div>
+    <>
+    {
+      isShowTable && <p>table</p>
+      // <>
+      //   <tr>
+      //     <td>table 1</td>
+      //     <td>table 2</td>
+      //     <td>table 3</td>
+      //   </tr>
+      //   <tr>
+      //     <td>table 1</td>
+      //     <td>table 2</td>
+      //     <td>table 3</td>
+      //   </tr>
+      // </>
+    }
+    </>
   )
 }
