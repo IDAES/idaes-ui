f24244b46a6639426d3311caa7e56d6f4b2068d9
CopyDemon <shengdevelop@gmail.com>
2023-07-19 10:42:12 -0700

    testing render components to FV component

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_component/flowsheet_component.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_component/flowsheet_component.tsx
@@ -24,21 +24,31 @@ export default function Flowsheet(){
         });
 
         var rect = new joint.shapes.standard.Rectangle();
-        rect.position(100, 30);
+        rect.position(400, 250);
         rect.resize(100, 40);
         rect.attr({
             body: {
-                fill: 'blue'
+                fill: 'none'
             },
             label: {
-                text: 'Flowsheet',
-                fill: 'white'
+                text: 'inlet_1_1',
+                fill: 'black'
             }
         });
         rect.addTo(graph);
+        
+        //ract 1
+        var rect1 = rect.clone();
+        rect1.translate(0, 200);
+        rect1.attr('label/text', 'inlet_2_1');
+        rect1.addTo(graph);
 
+        var link = new joint.shapes.standard.Link();
+        link.addTo(graph);
+
+        //ract 2
         var rect2 = rect.clone();
-        rect2.translate(300, 0);
+        rect2.translate(250, 100);
         rect2.attr('label/text', 'M01');
         rect2.addTo(graph);
 
@@ -46,12 +56,61 @@ export default function Flowsheet(){
         link.source(rect);
         link.target(rect2);
         link.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect1);
+        link.target(rect2);
+        link.addTo(graph);
+
+        //ract 3
+        var rect3 = rect.clone();
+        rect3.translate(500, 100);
+        rect3.attr('label/text', 'H02');
+        rect3.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect2);
+        link.target(rect3);
+        link.addTo(graph);
+
+        //ract 4
+        var rect4 = rect.clone();
+        rect4.translate(750, 100);
+        rect4.attr('label/text', 'F03');
+        rect4.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect3);
+        link.target(rect4);
+        link.addTo(graph);
+
+        //ract 5
+        var rect5 = rect.clone();
+        rect5.translate(1000, 0);
+        rect5.attr('label/text', 'vap_outlet_1');
+        rect5.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect4);
+        link.target(rect5);
+        link.addTo(graph);
+
+        //ract 6
+        var rect6 = rect.clone();
+        rect6.translate(1000, 200);
+        rect6.attr('label/text', 'liq_outlet_2');
+        rect6.addTo(graph);
+
+        var link = new joint.shapes.standard.Link();
+        link.source(rect4);
+        link.target(rect6);
+        link.addTo(graph);
   
   return(
     <>
       {
         isShowFlowsheet &&
-        <section>
+        <section className="pd-md">
           flowsheet
           <div id="myholder"></div>
           <div className="canvas" ref={canvas}></div>
