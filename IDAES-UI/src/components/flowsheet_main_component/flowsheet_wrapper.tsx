100b883ea52936e263e253000eafb1f7d2307966
CopyDemon <shengdevelop@gmail.com>
2023-07-19 10:41:32 -0700

    update panel group and handle size

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_wrapper.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_wrapper.tsx
@@ -1,16 +1,20 @@
 import { PanelGroup, Panel, PanelResizeHandle} from "react-resizable-panels";
+import FlowsheetHeader from "./flowsheet_component/flowsheet_header/flowsheet_header_component";
 import Flowsheet from "./flowsheet_component/flowsheet_component";
+import FlowsheetVariablesHeader from "./flowsheet_variables/flowsheet_variables_header/flowsheet_variables_header";
 import Flowsheet_variable from "./flowsheet_variables/flowsheet_variable_component";
 
 export default function FlowsheetWrapper(){
   return(
     <>
       <PanelGroup direction="horizontal">
-        <Panel defaultSize={70} minSize={20}>
+        <Panel defaultSize={70} minSize={26.1}>
+          <FlowsheetHeader />
           <Flowsheet />
         </Panel>
-        <PanelResizeHandle style={{width: "10px", backgroundColor: "black", marginLeft : "20px", marginRight: "20px"}}/>
-        <Panel minSize={30}>
+        <PanelResizeHandle className="panelResizeHandle panelResizeHandle_vertical"/>
+        <Panel minSize={26.1}>
+          <FlowsheetVariablesHeader />
           <Flowsheet_variable />
         </Panel>
       </PanelGroup>
