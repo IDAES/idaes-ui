ce321019fcd472574b5e93086518ef081ca35939
CopyDemon <shengdevelop@gmail.com>
2023-07-23 14:37:11 -0700

    read context to handle expand btn toggle change content and icon in button

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_variables/flowsheet_variables_header/flowsheet_variables_header.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_variables/flowsheet_variables_header/flowsheet_variables_header.tsx
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
+import { AppContext } from "../../../../context/appMainContext"
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpRightAndDownLeftFromCenter, faMinus, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faUpRightAndDownLeftFromCenter, faMinus, faCaretUp} from '@fortawesome/free-solid-svg-icons';
 
 import css from "./flowsheet_variables_header.module.css";
 
 export default function FlowsheetVariablesHeader(){
+  const variablesExpand = useContext(AppContext).variablesExpandState;
+  const { expandVariablesHandler } = useContext(AppContext)
+
   return(
     <div className={`pd-md ${css.flowsheetVariablesHeader_main_container}`}>
       <p className={`${css.flowsheetVariables_title}`}>VARIABLES</p>
       <div className={css.flowsheetVariablesHeader_icon_container}>
-        <span className={`${css.flowsheetVariablesHeader_icon} ${css.flowsheetVariableHeader_collapse_btn}`}>
-          <FontAwesomeIcon icon={faCaretUp} rotation={180} />
-          Collapse
+        <span className={`${css.flowsheetVariablesHeader_icon} ${css.flowsheetVariableHeader_collapse_btn}`}
+              onClick={expandVariablesHandler}
+        >
+          <FontAwesomeIcon icon={faCaretUp} rotation={variablesExpand.expand ? 180 : 90} />
+          {variablesExpand.expand ? "Collapse" : "Expand"}
         </span>
         <span className={`pd-sm ${css.flowsheetVariablesHeader_icon} ${css.flowsheetVariablesHeader_small_icon}`}>
           <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
