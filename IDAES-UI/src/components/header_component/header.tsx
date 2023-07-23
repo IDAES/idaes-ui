f2f45c8f9182943506942750b8a191bf0961f60c
CopyDemon <shengdevelop@gmail.com>
2023-07-23 14:47:45 -0700

    import header.css for all common elements in header

--- IDAES-UI/src/components/header_component/header.tsx
+++ IDAES-UI/src/components/header_component/header.tsx
@@ -5,6 +5,7 @@ import HeaderLogo from "./header_logo_component/header_logo";
 import HeaderFlowsheetName from "./header_flowsheet_name_component/header_flowsheet_name";
 import HeaderFunctionButtonsWrapper from "./header_fn_btn_wrapper_component/hader_fn_btn_wrapper";
 
+import "./header.css";
 import css from "./header.module.css";
 
 export default function Header(){
