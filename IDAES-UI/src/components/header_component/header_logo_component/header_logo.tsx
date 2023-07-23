16d535b02d49f1e530f9c3c01c3045b6d105a778
CopyDemon <shengdevelop@gmail.com>
2023-07-23 14:55:47 -0700

    update header logo as design file logo no text with IDAES text use section to wrap around

--- IDAES-UI/src/components/header_component/header_logo_component/header_logo.tsx
+++ IDAES-UI/src/components/header_component/header_logo_component/header_logo.tsx
@@ -2,6 +2,13 @@ import css from './header_logo.module.css';
 
 export default function HeaderLogo() {
   return(
-    <img src="/assets/image/idaes-logo.png" alt="idaes logo for header component" className={`${css.header_logo}`}/>
+    <section id="headerLogoContainer" className={`headerLogoContainer ${css.headerLogoContainer}`}>
+      <img src="/assets/image/idaes-logo.png" 
+          alt="idaes logo for header component" 
+          id="headerLogo" 
+          className={`${css.headerLogo}`}
+      />
+      <p id="headerLogoText" className={`${css.headerLogoText}`}>IDAES</p>
+    </section>
   )
 }
