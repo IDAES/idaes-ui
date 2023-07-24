ad984449169dd513c4982e803923c13e16874dac
CopyDemon <shengdevelop@gmail.com>
2023-07-24 14:29:27 -0700

    create interface for fvHeaderState

--- IDAES-UI/src/interface/appMainContext_interface.tsx
+++ IDAES-UI/src/interface/appMainContext_interface.tsx
@@ -2,3 +2,8 @@ export interface panelStateInterface{
   panelName : string,
   show : boolean
 }
+
+export interface fvHeaderStateInterface {
+  isShowSteamName : boolean,
+  isShowLabels : boolean
+}
