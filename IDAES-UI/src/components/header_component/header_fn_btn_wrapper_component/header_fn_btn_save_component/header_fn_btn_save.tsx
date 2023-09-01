f9350e0a57e0f1ba867c2cc76ea7cc7d6a3c3d9d
CopyDemon <shengdevelop@gmail.com>
2023-09-01 12:48:03 -0700

    add id to save btn to let toobar class can register event

--- IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_save_component/header_fn_btn_save.tsx
+++ IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_save_component/header_fn_btn_save.tsx
@@ -4,7 +4,7 @@ import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 export default function HeaderFNBtnSave(){
   
   return(
-    <li className={`header_each_btn`}>
+    <li id="save_btn" className={`header_each_btn`}>
         <FontAwesomeIcon icon={faFloppyDisk} className="mr-sm"/>
         Save
     </li>
