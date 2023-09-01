c6709450278a1e168140e3e3ecaf7d800b17c4b8
CopyDemon <shengdevelop@gmail.com>
2023-09-01 10:11:25 -0700

    remove log fn in save btn, enable save fn later

--- IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_save_component/header_fn_btn_save.tsx
+++ IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_save_component/header_fn_btn_save.tsx
@@ -2,12 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 
 export default function HeaderFNBtnSave(){
-  function saveHandler(){
-    console.log(`saved`)
-  }
   
   return(
-    <li className={`header_each_btn`} onClick={saveHandler}>
+    <li className={`header_each_btn`}>
         <FontAwesomeIcon icon={faFloppyDisk} className="mr-sm"/>
         Save
     </li>
