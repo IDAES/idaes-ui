8a3c81269cc722ed8d514b3709975b8123abfdc0
CopyDemon <shengdevelop@gmail.com>
2023-09-10 19:56:17 -0700

    add new component minimized_bar

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_wrapper.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_wrapper.tsx
@@ -3,6 +3,7 @@ import {AppContext} from "../../context/appMainContext";
 
 import { PanelGroup, Panel, PanelResizeHandle} from "react-resizable-panels";
 import FlowsheetHeader from "./flowsheet_component/flowsheet_header/flowsheet_header_component";
+import MinimizedBar from "./minimized_bar_component/minimized_bar_component";
 import Flowsheet from "./flowsheet_component/flowsheet_component";
 import FlowsheetVariablesHeader from "./flowsheet_variables/flowsheet_variables_header/flowsheet_variables_header";
 import Flowsheet_variable from "./flowsheet_variables/flowsheet_variable_component";
@@ -28,6 +29,7 @@ export default function FlowsheetWrapper(){
 
   return(
     <div id="flowsheet-wrapper" className={css.flowsheetWrapper}>
+      <MinimizedBar />
       <PanelGroup direction="vertical" id="flowsheet-wrapper">
         {
           isFvShow &&
