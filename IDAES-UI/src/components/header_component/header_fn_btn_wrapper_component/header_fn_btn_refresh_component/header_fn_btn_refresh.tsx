47f6399b8a5bd28a821336f9b25b75018e31290b
CopyDemon <shengdevelop@gmail.com>
2023-09-11 14:48:40 -0700

    remove refresh btn fn in react handler now in mainfv class, add id as selector

--- IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_refresh_component/header_fn_btn_refresh.tsx
+++ IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_refresh_component/header_fn_btn_refresh.tsx
@@ -1,12 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 export default function HeaderFNBtnRefresh(){
-  function refreshHandler(){
-    console.log(`refreshed header_fn_btn_refresh`)
-  }
-  
   return(
-    <li className={`header_each_btn`} onClick={refreshHandler}>
+    <li id="refresh_btn" className={`header_each_btn`}>
       <FontAwesomeIcon icon={faArrowsRotate} className="mr-sm"/>
       Refresh
     </li>
