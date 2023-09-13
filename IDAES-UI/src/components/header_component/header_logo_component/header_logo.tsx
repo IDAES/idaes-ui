ed7e3000bf2245a5950926929ccfd394c23b1468
CopyDemon <shengdevelop@gmail.com>
2023-09-13 09:45:48 -0700

    use import not path to link to image

--- IDAES-UI/src/components/header_component/header_logo_component/header_logo.tsx
+++ IDAES-UI/src/components/header_component/header_logo_component/header_logo.tsx
@@ -1,9 +1,10 @@
 import css from './header_logo.module.css';
+import idaes_logo from '../../../assets/images/idaes-logo.png';
 
 export default function HeaderLogo() {
   return(
     <section id="headerLogoContainer" className={`headerLogoContainer ${css.headerLogoContainer}`}>
-      <img src="/assets/image/idaes-logo.png" 
+      <img src={idaes_logo} 
           alt="idaes logo for header component" 
           id="headerLogo" 
           className={`${css.headerLogo}`}
