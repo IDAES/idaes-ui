00ba3cc25799c517386784a6955ece00c5aaeb1e
Dan Gunter <dkgunter@lbl.gov>
2023-09-14 07:30:15 -0700

    more better now

--- idaes_ui/util/diagnostics.py
+++ idaes_ui/util/diagnostics.py
@@ -6,14 +6,15 @@ for easier consumption and use by the UI layer.
 __author__ = "Dan Gunter"
 __created__ = "2023-09-05"
 
-from abc import ABC, abstractmethod
-from typing import Union, Sequence, Tuple
+import re
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel
 
 # Import IDAES functionality (use shortened names, i=IDAES m=model)
 from idaes.core.util import model_statistics as ims
 from idaes.core.util import model_diagnostics as imd
+from idaes.core.util.scaling import get_jacobian, jacobian_cond
+
 # Import other needed IDAES types
 from idaes.core import FlowsheetBlock
 from pyomo.environ import Component
@@ -77,34 +78,37 @@ class ModelStatistics(BaseModel):
     # expressions
     num_expr: int = 0
 
-    def update(self, block: FlowsheetBlock):
-        """Update values in report from the input block.
+    def __init__(self, block: _BlockData):
+        super().__init__()
+        self._block = block
+        self.update()
 
-        Parameters:
-            block: Block to analyze
+    def update(self):
+        """Update values in report.
         """
+        b = self._block
         try:
             # variables
-            self.dof = ims.degrees_of_freedom(block)
-            self.num_var = ims.number_variables(block)
-            self.num_var_fixed = ims.number_fixed_variables(block)
-            self.num_var_unused = ims.number_unused_variables(block)
-            self.num_var_fixed_unused = ims.number_fixed_unused_variables(block)
-            self.num_var_ineq = ims.number_variables_only_in_inequalities(block)
-            self.num_var_fixed_ineq = ims.number_fixed_variables_only_in_inequalities(block)
+            self.dof = ims.degrees_of_freedom(b)
+            self.num_var = ims.number_variables(b)
+            self.num_var_fixed = ims.number_fixed_variables(b)
+            self.num_var_unused = ims.number_unused_variables(b)
+            self.num_var_fixed_unused = ims.number_fixed_unused_variables(b)
+            self.num_var_ineq = ims.number_variables_only_in_inequalities(b)
+            self.num_var_fixed_ineq = ims.number_fixed_variables_only_in_inequalities(b)
             # constraints / objectives
-            self.num_constr = ims.number_total_constraints(block)
-            self.num_constr_ineq = ims.number_total_equalities(block)
-            self.num_constr_deact_eq = ims.number_deactivated_equalities(block)
-            self.num_constr_ineq = ims.number_total_inequalities(block)
-            self.num_constr_deact_ineq = ims.number_deactivated_inequalities(block)
-            self.num_obj = ims.number_total_objectives(block)
-            self.num_obj_deact = ims.number_deactivated_objectives(block)
+            self.num_constr = ims.number_total_constraints(b)
+            self.num_constr_ineq = ims.number_total_equalities(b)
+            self.num_constr_deact_eq = ims.number_deactivated_equalities(b)
+            self.num_constr_ineq = ims.number_total_inequalities(b)
+            self.num_constr_deact_ineq = ims.number_deactivated_inequalities(b)
+            self.num_obj = ims.number_total_objectives(b)
+            self.num_obj_deact = ims.number_deactivated_objectives(b)
             # blocks
-            self.num_block = ims.number_total_blocks(block)
-            self.num_block_deact = ims.number_deactivated_blocks(block)
+            self.num_block = ims.number_total_blocks(b)
+            self.num_block_deact = ims.number_deactivated_blocks(b)
             # expressions
-            self.num_expr = ims.number_expressions(block)
+            self.num_expr = ims.number_expressions(b)
         except Exception as e:
             raise StatisticsUpdateError()
 
@@ -116,8 +120,8 @@ class ModelStatistics(BaseModel):
 diagnostics_index = (
     ("structural_issues", "Structural issues",
      "List of structural issues with model"),
-#    ("comp_out_of_range", "Comp. outside range",
-#     "List of model components outside of valid range"),
+    ("numerical_issues", "Numerical issues",
+     "List of numerical issues with model"),
 )
 
 
@@ -137,23 +141,64 @@ class StructuralIssues(BaseModel):
     next_steps: list[str]
 
 
-class ModelDiagnostics:
+class NumericalIssues(BaseModel):
+    warnings: list[str]
+    cautions: list[str]
+    next_steps: list[str]
+    jacobian_cond: float
+
+class ModelDiagnosticsRunner:
     """Interface to the IDAES `model_diagnostics` module.
     """
     def __init__(self, block: _BlockData, **kwargs):
         self.block = block
         self.tb = imd.DiagnosticsToolbox(block, **kwargs)
 
+    _warnings_expr = re.compile(r"WARNING:\s+(.*)")
+    _cautions_expr = re.compile(r"Caution:\s+(.*)")
+
+    def _clean_messages(self, warnings, cautions):
+        cleaned_warnings = []
+        for item in warnings:
+            m = self._warnings_expr.match(item)
+            cleaned_warnings.append(m.group(1) if m else item)
+        cleaned_cautions = []
+        for item in cautions:
+            m = self._cautions_expr.match(item)
+            cleaned_cautions.append(m.group(1) if m else item)
+        return cleaned_warnings, cleaned_cautions
+
     @property
     def structural_issues(self) -> StructuralIssues:
-        warnings, next_steps = self.tb._collect_structural_warnings()
-        cautions = self.tb._collect_structural_cautions()
+        """Compute and return structural issues with the model.
+        """
+        try:
+            warnings, next_steps = self.tb._collect_structural_warnings()
+            cautions = self.tb._collect_structural_cautions()
+        except Exception as e:
+            raise DiagnosticsError("structural_issues",
+                                   details=f"while getting warnings and cautions: {e}")
+        warnings, cautions = self._clean_messages(warnings, cautions)
         return StructuralIssues(warnings=warnings, cautions=cautions,
                                 next_steps=next_steps)
 
-    # def comp_out_of_range(self):
-    #     comp_list = imd.list_components_with_values_outside_valid_range(
-    #         self.block
-    #     )
-    #     return [] # XXX: Serialize
+    @property
+    def numerical_issues(self) -> NumericalIssues:
+        """Compute and return numerical issues with the model.
+        """
+        try:
+            jac, nlp = get_jacobian(self.block, scaled=False)
+        except Exception as e:
+            raise DiagnosticsError("numerical_issues",
+                                   details=f"from get_jacobian(): {e}")
+        try:
+            warnings, next_steps = self.tb._collect_numerical_warnings(jac=jac, nlp=nlp)
+            cautions = self.tb._collect_numerical_cautions(jac=jac, nlp=nlp)
+        except Exception as e:
+            raise DiagnosticsError("numerical_issues",
+                                   details=f"while getting warnings and cautions: {e}")
+        warnings, cautions = self._clean_messages(warnings, cautions)
+        return NumericalIssues(warnings=warnings, cautions=cautions,
+                                next_steps=next_steps,
+                               jacobian_cond=jacobian_cond(jac=jac, scaled=False))
 
--- idaes_ui/util/tests/test_diagnostics.py
+++ idaes_ui/util/tests/test_diagnostics.py
@@ -2,27 +2,28 @@
 Tests for the diagnostics wrapper
 """
 import pytest
-from idaes_ui.util.diagnostics import ModelStatistics, ModelDiagnostics, ModelInfo
-from idaes_ui.util.diagnostics import StatisticsUpdateError, DiagnosticsUpdateError
-from idaes_ui.fv.tests.flowsheets import flash_flowsheet
-
-
-@pytest.mark.unit
-def test_empty_stats():
-    r = ModelStatistics()
-    assert r.dof == 0
-    assert r.get_display_name("dof") == "Degrees of Freedom"
+from idaes_ui.util.diagnostics import ModelStatistics, ModelDiagnosticsRunner
+from idaes_ui.fv.tests.flowsheets import flash_flowsheet, demo_flowsheet
 
 
 @pytest.mark.unit
 def test_flash_stats():
     stats = ModelStatistics(block=flash_flowsheet())
-    # make sure num vars is within order of magnitude
+    # check that num vars is right order of magnitude
     assert 10 < stats.num_var < 100
 
 
 @pytest.mark.unit
 def test_structural():
-    diag = ModelDiagnostics(block=flash_flowsheet())
+    diag = ModelDiagnosticsRunner(block=flash_flowsheet())
     data = diag.structural_issues
+    print(f"structural json: {data.model_dump_json()}")
     assert len(data.warnings) == 0
+
+
+@pytest.mark.unit
+def test_numerical():
+    diag = ModelDiagnosticsRunner(block=demo_flowsheet())
+    data = diag.numerical_issues
+    print(f"numerical json: {data.model_dump_json()}")
+    #assert len(data.warnings) == 0
