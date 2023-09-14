42c5c7b17ec74a4eb60c9f4895d99c44e83b4b62
Dan Gunter <dkgunter@lbl.gov>
2023-09-14 00:13:13 -0700

    simpler interface, exceptions

--- idaes_ui/util/diagnostics.py
+++ idaes_ui/util/diagnostics.py
@@ -6,40 +6,131 @@ for easier consumption and use by the UI layer.
 __author__ = "Dan Gunter"
 __created__ = "2023-09-05"
 
-from pydantic import BaseModel, Field
+from abc import ABC, abstractmethod
+from typing import Union, Sequence, Tuple
+
+from pydantic import BaseModel, Field, ConfigDict
 
 # Import IDAES functionality (use shortened names, i=IDAES m=model)
 from idaes.core.util import model_statistics as ims
 from idaes.core.util import model_diagnostics as imd
+# Import other needed IDAES types
 from idaes.core import FlowsheetBlock
 from pyomo.environ import Component
 
+# Exceptions
+# ----------
 
-class ModelStatistics(BaseModel):
+
+class UpdateError(Exception):
+    def __init__(self, what, details=None):
+        msg = f"{what} update failed"
+        if details:
+            msg += f": {details}"
+        super().__init__(msg)
+
+
+class StatisticsUpdateError(UpdateError):
+    def __init__(self, **kwargs):
+        super().__init__("Statistics", **kwargs)
+
+
+class DiagnosticsUpdateError(UpdateError):
+    def __init__(self, **kwargs):
+        super().__init__("Diagnostics", **kwargs)
+
+# Constants
+# ---------
+
+
+META_NAME_SECTION = "names"
+META_DESC_SECTION = "descriptions"
+
+
+# Functions / Classes
+# -------------------
+
+class ModelBlock(BaseModel, ABC):
+    def __init__(self, block: FlowsheetBlock = None):
+        """Create new model.
+
+        Parameters:
+             block: If given, pass to `update()` to set initial values.
+        """
+        super().__init__()
+        if block:
+            self.update(block)
+
+    @abstractmethod
+    def update(self, block: FlowsheetBlock):
+        pass
+
+
+class HasMetadata(BaseModel):
+    """Provide an interface to metadata for attributes.
+    """
+    meta_: dict = Field(default={META_NAME_SECTION: {}, META_DESC_SECTION: {}})
+
+    def get_display_name(self, attr) -> Union[str, None]:
+        """Get display name for given attribute.
+
+        Args:
+            attr: Name of attribute (should match name in class)
+
+        Returns:
+            name or None if no name was provided in the metadata
+        """
+        return self._get_meta(META_NAME_SECTION, attr, None)
+
+    def get_desc(self, attr) -> str:
+        """Get description for given attribute.
+
+        Args:
+            attr: Name of attribute (should match name in class)
+
+        Returns:
+            description or empty string if none was provided in the metadata
+        #"""
+        return self._get_meta(META_DESC_SECTION, attr, "")
+
+    def _get_meta(self, section, attr, empty):
+        if attr not in self.meta_[section]:
+            return empty
+        return self.meta_[section][attr]
+
+    def set_names(self, items: Sequence[Tuple[str, str]]):
+        for key, value in items:
+            self.meta_[META_NAME_SECTION][key] = value
+
+    def set_descriptions(self, items: Sequence[Tuple[str, str]]):
+        for key, value in items:
+            self.meta_[META_DESC_SECTION][key] = value
+
+
+class ModelStatistics(ModelBlock, HasMetadata):
+    """Interface to the IDAES `model_statistics` module.
+    """
     # variables
-    dof: int = Field(default=0, alias="Degrees of Freedom")
-    num_var: int = Field(default=0, alias="Total No. of Variables")
-    num_var_fixed: int = Field(default=0, alias="No. Fixed Variables")
-    num_var_unused: int = Field(default=0, alias="No. Unused Variables")
-    num_var_fixed_unused: int = Field(default=0, alias="No. Unused Variables (Fixed)")
-    num_var_ineq: int = Field(default=0, alias="No. Variables only in Inequalities")
-    num_var_fixed_ineq: int = Field(default=0,
-                                    alias="No. Variables only in Inequalities (Fixed)")
+    dof: int = 0  # degrees of freedom
+    num_var: int = 0
+    num_var_fixed: int = 0
+    num_var_unused: int = 0
+    num_var_fixed_unused: int = 0
+    num_var_ineq: int = 0
+    num_var_fixed_ineq: int = 0
     # constraints/objectives
-    num_constr: int = Field(default=0, alias="Total No. Constraints")
-    num_constr_eq: int = Field(default=0, alias="No. Equality Constraints")
-    num_constr_deact_eq: int = Field(default=0,
-                                     alias="No. Equality Constraints (Deactivated)")
-    num_constr_ineq: int = Field(default=0, alias="No. Inequality Constraints")
-    num_constr_deact_ineq: int = Field(default=0,
-                                       alias="No. Inequality Constraints (Deactivated)")
-    num_obj: int = Field(default=0, alias="No. Objectives")
-    num_obj_deact: int = Field(default=0, alias="No. Objectives (Deactivated)")
+    num_constr: int = 0
+    num_constr_eq: int = 0
+    num_constr_deact_eq: int = 0
+    num_constr_ineq: int = 0
+    num_constr_deact_ineq: int = 0
+    num_obj: int = 0
+    num_obj_deact: int = 0
     # blocks
-    num_block: int = Field(default=0, alias="No. Blocks")
-    num_block_deact: int = Field(default=0, alias="No. Blocks (Deactivated)")
+    num_block: int = 0
+    num_block_deact: int = 0
     # expressions
-    num_expr: int = Field(default=0, alias="No. Expressions")
+    num_expr: int = 0
 
     def __init__(self, block: FlowsheetBlock = None):
         """Create new model.
@@ -47,9 +138,24 @@ class ModelStatistics(BaseModel):
         Parameters:
              block: If given, pass to `update()` to set initial values.
         """
-        BaseModel.__init__(self)
-        if block:
-            self.update(block)
+        super().__init__(block)
+        self.set_names((
+                ("dof", "Degrees of Freedom"),
+                ("num_var", "Total No. of Variables"),
+                ("num_var_fixed", "No. Fixed Variables"),
+                ("num_var_unused", "No. Unused Variables"),
+                ("num_var_fixed_unused", "No. Unused Variables (Fixed)"),
+                ("num_var_ineq", "No. Variables only in Inequalities"),
+                ("num_var_fixed_ineq", "No. Variables only in Inequalities (Fixed)"),
+                ("num_constr", "Total No. Constraints"),
+                ("num_constr_eq", "No. Equality Constraints"),
+                ("num_constr_deact_eq", "No. Equality Constraints (Deactivated)"),
+                ("num_obj", "No. Objectives"),
+                ("num_obj_deact", "No. Objectives (Deactivated)"),
+                ("num_block", "No. Blocks"),
+                ("num_block_deact", "No. Blocks (Deactivated)"),
+                ("num_expr", "No. Expressions"),
+        ))
 
     def update(self, block: FlowsheetBlock):
         """Update values in report from the input block.
@@ -57,35 +163,59 @@ class ModelStatistics(BaseModel):
         Parameters:
             block: Block to analyze
         """
-        # variables
-        self.dof = ims.degrees_of_freedom(block)
-        self.num_var = ims.number_variables(block)
-        self.num_var_fixed = ims.number_fixed_variables(block)
-        self.num_var_unused = ims.number_unused_variables(block)
-        self.num_var_fixed_unused = ims.number_fixed_unused_variables(block)
-        self.num_var_ineq = ims.number_variables_only_in_inequalities(block)
-        self.num_var_fixed_ineq = ims.number_fixed_variables_only_in_inequalities(block)
-        # constraints / objectives
-        self.num_constr = ims.number_total_constraints(block)
-        self.num_constr_ineq = ims.number_total_equalities(block)
-        self.num_constr_deact_eq = ims.number_deactivated_equalities(block)
-        self.num_constr_ineq = ims.number_total_inequalities(block)
-        self.num_constr_deact_ineq = ims.number_deactivated_inequalities(block)
-        self.num_obj = ims.number_total_objectives(block)
-        self.num_obj_deact = ims.number_deactivated_objectives(block)
-        # blocks
-        self.num_block = ims.number_total_blocks(block)
-        self.num_block_deact = ims.number_deactivated_blocks(block)
-        # expressions
-        self.num_expr = ims.number_expressions(block)
+        try:
+            # variables
+            self.dof = ims.degrees_of_freedom(block)
+            self.num_var = ims.number_variables(block)
+            self.num_var_fixed = ims.number_fixed_variables(block)
+            self.num_var_unused = ims.number_unused_variables(block)
+            self.num_var_fixed_unused = ims.number_fixed_unused_variables(block)
+            self.num_var_ineq = ims.number_variables_only_in_inequalities(block)
+            self.num_var_fixed_ineq = ims.number_fixed_variables_only_in_inequalities(block)
+            # constraints / objectives
+            self.num_constr = ims.number_total_constraints(block)
+            self.num_constr_ineq = ims.number_total_equalities(block)
+            self.num_constr_deact_eq = ims.number_deactivated_equalities(block)
+            self.num_constr_ineq = ims.number_total_inequalities(block)
+            self.num_constr_deact_ineq = ims.number_deactivated_inequalities(block)
+            self.num_obj = ims.number_total_objectives(block)
+            self.num_obj_deact = ims.number_deactivated_objectives(block)
+            # blocks
+            self.num_block = ims.number_total_blocks(block)
+            self.num_block_deact = ims.number_deactivated_blocks(block)
+            # expressions
+            self.num_expr = ims.number_expressions(block)
+        except Exception as e:
+            raise StatisticsUpdateError()
 
 
-class ModelDiagnostics(BaseModel):
-    comp_out_of_range: list[Component] = Field(dafault=None,
-                                               alias="Comp. outside range")
+class ModelDiagnostics(ModelBlock, HasMetadata):
+    """Interface to the IDAES `model_diagnostics` module.
+    """
+    comp_out_of_range: list[Component] = []
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def __init__(self, block: FlowsheetBlock = None):
+        super().__init__(block)
+        self.set_names((
+            ("comp_out_of_range", "Comp. outside range"),
+        ))
+        self.set_descriptions((
+            ("comp_out_of_range", "Components outside of valid range"),
+        ))
 
     def update(self, block: FlowsheetBlock):
-        """Update values from the input block.
-        """
-        self.comp_out_of_range = imd.list_components_with_values_outside_valid_range(
-            block)
+        """Update values from the input block."""
+        try:
+            self.comp_out_of_range = imd.list_components_with_values_outside_valid_range(
+                block
+            )
+        except Exception as e:
+            raise DiagnosticsUpdateError()
+
+
+class ModelInfo(BaseModel):
+    """Combined model statistics and diagnostics
+    """
+    statistics: ModelStatistics
+    diagnostics: ModelDiagnostics
--- idaes_ui/util/tests/test_diagnostics.py
+++ idaes_ui/util/tests/test_diagnostics.py
@@ -2,17 +2,43 @@
 Tests for the diagnostics wrapper
 """
 import pytest
-from idaes_ui.util.diagnostics import ModelStatistics
+from idaes_ui.util.diagnostics import ModelStatistics, ModelDiagnostics, ModelInfo
+from idaes_ui.util.diagnostics import StatisticsUpdateError, DiagnosticsUpdateError
 from idaes_ui.fv.tests.flowsheets import flash_flowsheet
 
 
 @pytest.mark.unit
-def test_create_report():
+def test_empty_stats():
     r = ModelStatistics()
     assert r.dof == 0
+    assert r.get_display_name("dof") == "Degrees of Freedom"
+
+
+def test_empty_diagnostics():
+    d = ModelDiagnostics()
+    assert d.comp_out_of_range == []
+    assert "outside range" in d.get_display_name("comp_out_of_range")
 
 
 @pytest.mark.unit
-def test_update_report():
+def test_flash_stats():
     stats = ModelStatistics(block=flash_flowsheet())
-    print(f"report for flash flowsheet: {stats.model_dump_json(by_alias=True)}")
+    # make sure num vars is within order of magnitude
+    assert 10 < stats.num_var < 100
+
+
+@pytest.mark.unit
+def test_flash_diagnostics():
+    with pytest.raises(DiagnosticsUpdateError):
+        d = ModelDiagnostics("foo")
+
+    d = ModelDiagnostics(block=flash_flowsheet())
+
+
+@pytest.mark.unit
+def test_empty_info():
+    i = ModelInfo(statistics=ModelStatistics(),
+                  diagnostics=ModelDiagnostics())
+    assert i.statistics.dof == 0
+    assert i.diagnostics.comp_out_of_range == []
+    print(f"@@ INFO::{i.model_dump_json()}")
