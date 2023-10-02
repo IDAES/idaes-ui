cccf6ab68e4470fd0cfb60614d464802acfa7713
CopyDemon <shengdevelop@gmail.com>
2023-10-02 16:35:15 -0700

    make basicstructure of diagnostic panel with header and fake panel

---
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
@@ -0,0 +1,9 @@
+import DiagnosticsHeader from "./flowsheet_diagnostics_header/flowsheet_diagnostics_header"
+export default function FlowsheetDiagnostics(){
+    return (
+        <>
+            <DiagnosticsHeader />
+            <p>diagnostics</p>
+        </>
+    )
+}
