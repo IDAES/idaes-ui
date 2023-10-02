a3d250ab24a9d6ba86d54d9e06c2633cb0067c19
CopyDemon <shengdevelop@gmail.com>
2023-10-02 16:13:22 -0700

    initial diagnostics header btn and add toggle diagnostics panel handler

--- IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_diagnostics_component/header_fn_btn_diagnostics_component.tsx
+++ IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/header_fn_btn_diagnostics_component/header_fn_btn_diagnostics_component.tsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { AppContext } from "@/context/appMainContext";
 
-export default function HeaderFNBtnExportImage(){
+export default function HeaderFNBtnDiagnostics(){
+    const {panelState, setPanelState} = useContext(AppContext);
+    function toggleDiagnosticHandler(){
+        setPanelState((prev:any)=>{
+            const copyState = {...prev};
+            copyState.diagnostics.show = !copyState.diagnostics.show;
+            return copyState;
+        })
+    }
     return(
-        <li id="headerDiagnosticsBtn" className={`header_each_btn`}>
+        <li 
+            id="headerDiagnosticsBtn" 
+            className={`header_each_btn`}
+            onClick={toggleDiagnosticHandler}
+        >
             <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-sm"/>
             Diagnostics
         </li>
