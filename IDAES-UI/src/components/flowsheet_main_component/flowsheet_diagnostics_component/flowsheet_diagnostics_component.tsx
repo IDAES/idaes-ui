75792b6e4450fe493f0840929416cf7ff6ab1dd6
CopyDemon <shengdevelop@gmail.com>
2023-10-03 15:35:09 -0700

    create diagnostics components

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
@@ -1,9 +1,19 @@
-import DiagnosticsHeader from "./flowsheet_diagnostics_header/flowsheet_diagnostics_header"
+import { useEffect} from "react";
+import axios from "axios";
+import DiagnosticsHeader from "./flowsheet_diagnostics_header/flowsheet_diagnostics_header";
+import { Diagnostic_main } from "./diagnostic_functions/diagnostic_main";
+import "./flowsheet_diagnostics.css";
+
+
 export default function FlowsheetDiagnostics(){
+    useEffect(()=>{
+        let diagnosticDataURL = "/public/data/diagnostic_example.json";
+        let diagnosticMain = new Diagnostic_main(diagnosticDataURL);
+    },[])
     return (
         <>
             <DiagnosticsHeader />
-            <p>diagnostics</p>
+            <div id="diagnosticsContainer"></div>
         </>
     )
 }
