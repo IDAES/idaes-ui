00274533b486e93d4c1ce9d2c4bd92a5bfe8c3f6
CopyDemon <shengdevelop@gmail.com>
2023-11-10 22:55:29 -0800

    only run on local so enable all port can pass CORS

--- idaes_ui/fv/fastAPI_functions/cors.py
+++ idaes_ui/fv/fastAPI_functions/cors.py
@@ -6,16 +6,9 @@ def enable_fastapi_cors(app):
     Args:
         app: the class enables fastAPI
     """
-    origins = [
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ]
-
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,  # allowed url list
+        allow_origins=["*"],  # allowed url list
         allow_credentials=True,  # support cookies
         allow_methods=["*"],  # allowed methord
         allow_headers=["*"],  # allowed header
