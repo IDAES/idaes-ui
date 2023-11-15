1fc0305bc823d2a058d0a4f3fc21fb6744bdbf73
CopyDemon <shengdevelop@gmail.com>
2023-11-15 10:26:35 -0800

    work save put fs end point

--- idaes_ui/fv/fastAPI_route/api_put_flowsheet_route.py
+++ idaes_ui/fv/fastAPI_route/api_put_flowsheet_route.py
@@ -1,5 +1,15 @@
-class PutFlowsheetRoute:
+from pydantic import BaseModel
+
+
+class PutFlowsheetRoute(BaseModel):
+    updateFlowsheet: str
+    oldFlowsheet: str
+
     def __init__(self, fastAPIApp):
-        @fastAPIApp.put("/api/put_fs", tags=["Flowsheet"])
-        def put_flowsheet(updatedFlowsheet, oldFlowsheet):
-            return {"message": "put flowsheet is workiung"}
+        print("until here is working")
+
+        @fastAPIApp.put("/api/put_fs", tags=["Save Flowsheet"])
+        def put_flowsheet(flowsheet_data):
+            updated_flowsheet = flowsheet_data.updatedFlowsheet
+            old_flowsheet = flowsheet_data.oldFlowsheet
+            return {"message": updated_flowsheet}
