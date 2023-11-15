273d4656667d672d4caadafebec8f962eb46f6ee
CopyDemon <shengdevelop@gmail.com>
2023-11-15 13:09:21 -0800

    enable baisc put req for put fs

--- idaes_ui/fv/fastAPI_route/api_put_flowsheet_route.py
+++ idaes_ui/fv/fastAPI_route/api_put_flowsheet_route.py
@@ -1,15 +1,33 @@
 from pydantic import BaseModel
 
+from idaes_ui.fv.models.flowsheet import Flowsheet
 
-class PutFlowsheetRoute(BaseModel):
-    updateFlowsheet: str
-    oldFlowsheet: str
 
+class PutFlowsheetReqModel(BaseModel):
+    """Define PUT request model
+    Args:
+        BaseModel: pydantic BaseModel
+    Body_Params:
+        fs_name: string, the name of flowsheet
+        fs: Flowsheet Object, pass from frontend
+    """
+
+    fs_name: str
+    fs: Flowsheet
+
+
+class PutFlowsheetRoute:
     def __init__(self, fastAPIApp):
-        print("until here is working")
-
         @fastAPIApp.put("/api/put_fs", tags=["Save Flowsheet"])
-        def put_flowsheet(flowsheet_data):
-            updated_flowsheet = flowsheet_data.updatedFlowsheet
-            old_flowsheet = flowsheet_data.oldFlowsheet
-            return {"message": updated_flowsheet}
+        def put_flowsheet(req_body: PutFlowsheetReqModel):
+            """PUT request use to receive updated flowsheet, compare with stored flowsheet, if find change, update the stored one.
+            Args:
+                req_body : the new flowsheet pass from fronend
+            returns:
+                updated flowsheet
+            """
+            print("Recetive request from /api/put_fs......")
+
+            if not req_body.fs_name or not req_body.fs:
+                return {"error": "please check you missing body params"}
+            return req_body.fs
