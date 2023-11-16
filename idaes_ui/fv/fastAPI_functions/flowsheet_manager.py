27d1a5f9a1f3d3c69f15c5d015e495c9851528d6
CopyDemon <shengdevelop@gmail.com>
2023-11-16 15:33:35 -0800

    rename get update and original flowsheet name frontend use jjs format, diagnostic use original

--- idaes_ui/fv/fastAPI_functions/flowsheet_manager.py
+++ idaes_ui/fv/fastAPI_functions/flowsheet_manager.py
@@ -5,21 +5,25 @@ class FlowsheetManager:
     """Use to manage flowsheet, use as a universal container can allow different class call its getter ans setter to update and read new flowsheet"""
 
     def __init__(self, flowsheet):
-        """init assign self flowsheet = flowsheet
+        """init assign define slef's
         Args:
             flowsheet: the flowsheet pass eather from fsvis -> FlowsheetApp -> Router
         """
-        self.flowsheet = Flowsheet(flowsheet)
+        self.original_flowsheet = flowsheet
+        self.jjs_flowsheet = Flowsheet(flowsheet)
 
-    def get_flowsheet(self) -> Flowsheet:
-        """Return most recent flowsheet
+    def get_original_flowsheet(self):
+        return self.original_flowsheet
+
+    def get_jjs_flowsheet(self) -> Flowsheet:
+        """Return most recent flowsheet use in joint js
         Returns: flowsheet
         """
-        return self.flowsheet
+        return self.jjs_flowsheet
 
-    def update_flowsheet(self, new_flowsheet):
-        """Update self flowsheet to user saved flowsheet
+    def update_jjs_flowsheet(self, frontend_put_jjs_flowsheet):
+        """Update self flowsheet to user saved flowsheet use in joint js
         Args:
-            new_flowsheet: the flowsheet user saved and passed from api/put_fs
+            frontend_put_jjs_flowsheet: the flowsheet user saved and passed from api/put_fs
         """
-        self.flowsheet = new_flowsheet
+        self.jjs_flowsheet = frontend_put_jjs_flowsheet
