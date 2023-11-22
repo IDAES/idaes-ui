af24919b8e713cc769e7d7cc49134447dcde2752
CopyDemon <shengdevelop@gmail.com>
2023-11-22 12:05:41 -0800

    work save testing health check now return turn, will update later

--- idaes_ui/fv/fastAPI_route/api_post_health_check_route.py
+++ idaes_ui/fv/fastAPI_route/api_post_health_check_route.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 
 class HealthStatus(BaseModel):
-    alive: int
+    server_alive_check: int
 
 
 class PostHealthCheck:
@@ -12,7 +12,7 @@ class PostHealthCheck:
         @fastAPIApp.post("/api/post_health_check")
         def health_check(req_body: HealthStatus):
             # asyncio.create_task(self.stop_uvicorn_if_no_heartbeat())
-            return req_body.alive
+            return True
 
     # TODO: health check every 2 seconds front end will send a requst to present its tab is on if over 10 if over 10 seconds didn't receive that signal will turn that spcific uvicorn servic off
     # async def stop_uvicorn_if_no_heartbeat():
