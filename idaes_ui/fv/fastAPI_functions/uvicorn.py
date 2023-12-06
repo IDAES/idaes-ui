396ba5b6d972ad7c195c4bbb161cf44a3f05e833
CopyDemon <shengdevelop@gmail.com>
2023-12-06 15:06:09 -0800

    update port-> read port from parent class, remove port check move to server manager

--- idaes_ui/fv/fastAPI_functions/uvicorn.py
+++ idaes_ui/fv/fastAPI_functions/uvicorn.py
@@ -2,12 +2,9 @@
 Define how to use uvicorn to serve fastAPI app
 """
 
-import sys
-import socket
 import asyncio
 import uvicorn
 import webbrowser
-import threading
 from threading import Thread
 import time
 
@@ -16,7 +13,7 @@ class WebUvicorn:
     def __init__(self, fastAPIApp, port, flowsheet_name):
         self.flowsheet_name = flowsheet_name
         self.app = fastAPIApp
-        self.port = self.port_usage_check(port)
+        self.port = port
         # run uvicorn serve web app
         self.run()
 
@@ -30,7 +27,7 @@ class WebUvicorn:
             server: configed uvicorn server
         """
         # Get available port
-        self.port = self.port_usage_check(self.port)
+        self.port = self.port
         # setup uvicorn config
         config = uvicorn.Config(
             self.app, host="127.0.0.1", port=self.port  # loop="asyncio"
@@ -63,19 +60,3 @@ class WebUvicorn:
         Thread(target=self.run_server_async).start()
         time.sleep(1)  # give system 1 second to start server the open browser
         Thread(target=self.open_browser).start()  # start browser
-
-    def port_usage_check(self, port):
-        """use for port check, if pass in port is in use then modifiy port number + 1 until port available
-        Args:
-            port: the port use to pass in by user or default 8000
-        Returns:
-            port: the modified port number (available port number)
-        """
-
-        while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                try:
-                    s.bind(("127.0.0.1", port))
-                    return port
-                except OSError:
-                    port += 1
