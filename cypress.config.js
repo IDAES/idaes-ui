38b0dea2309eee2cea92fed8aaedfb0052a0e00e
CopyDemon <shengdevelop@gmail.com>
2023-12-11 10:49:32 -0800

    update cypress base url

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = {
   screenshots: true,
   video : false,
   e2e: {
-    baseUrl: "http://localhost:49999/app?id=sample_visualization",
+    baseUrl: "http://127.0.0.1:49999/?id=sample_visualization",
     pageLoadTimeout: 100000,
     taskTimeout: 100000,
     responseTimeout: 60000
