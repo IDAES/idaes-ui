f638915dd856b1420b9ee08984b70221d6c375e4
CopyDemon <shengdevelop@gmail.com>
2023-12-29 20:44:16 -0800

    use server manager instence return fastapi app instence for test file use

--- idaes_ui/fv/fsvis.py
+++ idaes_ui/fv/fsvis.py
@@ -102,17 +102,19 @@ def visualize(
         RuntimeError: If too many versions of the save file already exist. See :data:`MAX_SAVED_VERSIONS`.
     """
 
-    # Initialize IDAES logging
-    _init_logging(log_level)
+    # # Initialize IDAES logging
+    # _init_logging(log_level)
 
-    ServerManager(
+    # start server manager, store instence into variable
+    server_manager_instence = ServerManager(
         flowsheet=flowsheet,
         flowsheet_name=name,
         port=port,
         save_time_interval=save_time_interval,
     )
 
-    return "favis is running ok"
+    # return fastapi instence from server manager instence
+    return server_manager_instence.fastapi_app
 
     # Start the web server
     # if web_server is None:
@@ -123,7 +125,7 @@ def visualize(
     #     # web_server = FlowsheetServer(port=port)
     #     # web_server.add_setting("save_time_interval", save_time_interval)
     #     # web_server.start()
-    # 	if not quiet:
+    #     if not quiet:
     #         _log.info("Started visualization server")
     # else:
     #     _log.info(f"Using HTTP server on localhost, port {web_server.port}")
