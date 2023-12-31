4546b060f32b0cc4121654c18dd55cf6cac0d08a
CopyDemon <shengdevelop@gmail.com>
2023-12-30 19:56:14 -0800

    1. move read flowsheet flowsheet name from flowsheet manager into if. 2. add get which return original_flowsheet

--- idaes_ui/fv/fastAPI_route/api_get_flowsheet_route.py
+++ idaes_ui/fv/fastAPI_route/api_get_flowsheet_route.py
@@ -9,10 +9,14 @@ class GetFlowsheetRoute:
     def __init__(self, fastAPIApp, flowsheet_manager, flowsheet_name, save_dir):
         @fastAPIApp.get("/api/get_fs", tags=["Flowsheet"])
         def get_flowsheet(get_which: str = Query(...)):
-            jjs_flowsheet = flowsheet_manager.get_jjs_flowsheet()
-            flowsheet_name = flowsheet_manager.get_flowsheet_name()
-
             if get_which == "flowsheet":
+                jjs_flowsheet = flowsheet_manager.get_jjs_flowsheet()
                 return jjs_flowsheet
+
             if get_which == "flowsheetName":
+                flowsheet_name = flowsheet_manager.get_flowsheet_name()
                 return flowsheet_name
+
+            if get_which == "original_flowsheet":
+                original_flowsheet = flowsheet_manager.get_original_flowsheet()
+                return original_flowsheet
