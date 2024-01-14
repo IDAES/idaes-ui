8384d1d2efbf1547b8eb66a499ef889fcdffb75e
CopyDemon <shengdevelop@gmail.com>
2024-01-13 17:59:06 -0800

    add fetch diagnostics data in and render sub component

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
@@ -1,20 +1,38 @@
-import { useEffect} from "react";
+import { useContext, useEffect, useState} from "react";
+import { AppContext } from "@/context/appMainContext";
 import axios from "axios";
 import DiagnosticsHeader from "./flowsheet_diagnostics_header/flowsheet_diagnostics_header";
 import { Diagnostic_main } from "./diagnostic_functions/diagnostic_main";
+import DiagnosticIssues from "./diagnostics_issues/diagnostics_issues";
 import "./flowsheet_diagnostics.css";
 
 
 export default function FlowsheetDiagnostics(){
+    const [diagnosticData, setDiagnosticsData] = useState(null);
+    let {server_port} = useContext(AppContext);
+    
     useEffect(()=>{
-        // let diagnosticDataURL = "/public/data/diagnostic_example.json";
-        let diagnosticDataURL = "/public/data/diagnostic_andrew.json";
-        let diagnosticMain = new Diagnostic_main(diagnosticDataURL);
-    },[])
+        const getDiagnosticUrl = `http://127.0.0.1:${server_port}/api/get_diagnostics`
+        // const fetchDiagnosticsData = axios.get(url);
+        // console.log(fetchDiagnosticsData);
+
+        const fetchDiagnosticData = async (url:string) =>{
+            try{
+                const res = await axios.get(url);
+                const data = res.data
+                setDiagnosticsData(data);
+            }catch(error){
+                console.error("Fetch diagnostic data error", error)
+            }
+        }
+        fetchDiagnosticData(getDiagnosticUrl);
+    },[]);
+
     return (
         <>
             <DiagnosticsHeader />
-            <div id="diagnosticsContainer"></div>
+            <DiagnosticIssues diagnosticData={diagnosticData}/>
+            {/* <div id="diagnosticsContainer"></div> */}
         </>
     )
 }
