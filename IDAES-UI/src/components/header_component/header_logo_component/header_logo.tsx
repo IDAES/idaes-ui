19b945a1ea7e07b73af9042e212a6d5e8506a17f
CopyDemon <shengdevelop@gmail.com>
2024-01-13 16:26:30 -0800

    move diagnostic btn to the left side of the header

--- IDAES-UI/src/components/header_component/header_logo_component/header_logo.tsx
+++ IDAES-UI/src/components/header_component/header_logo_component/header_logo.tsx
@@ -1,15 +1,21 @@
+import HeaderFNBtnDiagnostics from '../header_fn_btn_wrapper_component/header_fn_btn_diagnostics_component/header_fn_btn_diagnostics_component';
 import css from './header_logo.module.css';
 import idaes_logo from '../../../assets/images/idaes-logo.png';
 
 export default function HeaderLogo() {
   return(
-    <section id="headerLogoContainer" className={`headerLogoContainer ${css.headerLogoContainer}`}>
-      <img src={idaes_logo} 
-          alt="idaes logo for header component" 
-          id="headerLogo" 
-          className={`${css.headerLogo}`}
-      />
-      <p id="headerLogoText" className={`${css.headerLogoText}`}>IDAES</p>
-    </section>
+    <div className={css.headerLeftMainContainer}>
+      <div id="headerLogoContainer" className={`headerLogoContainer ${css.headerLogoContainer}`}>
+        <img src={idaes_logo} 
+            alt="idaes logo for header component" 
+            id="headerLogo" 
+            className={`${css.headerLogo}`}
+        />
+        <p id="headerLogoText" className={`${css.headerLogoText}`}>IDAES</p>
+      </div>
+      <div>
+        <HeaderFNBtnDiagnostics />
+      </div>
+    </div>
   )
 }
