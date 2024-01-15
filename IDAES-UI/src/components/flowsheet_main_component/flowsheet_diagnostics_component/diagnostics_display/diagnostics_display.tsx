5f9f600d6ba87e9406ee04a3e9a18cec066da363
CopyDemon <shengdevelop@gmail.com>
2024-01-14 20:11:45 -0800

    initial diagnostics display component

---
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/diagnostics_display/diagnostics_display.tsx
@@ -0,0 +1,9 @@
+import css from "./diagnostics_display.module.css";
+
+export default function DiagnosticsDisplay(props:any){
+    return(
+        <div className={`${css.diagnostics_display_main_container}`}>
+            main display
+        </div>
+    )
+}
