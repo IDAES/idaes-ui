ee5d0072d052922799397752e530d25631f154ce
CopyDemon <shengdevelop@gmail.com>
2024-01-24 15:43:56 -0800

    build module statistics output

--- idaes_ui/fv/models/issues.py
+++ idaes_ui/fv/models/issues.py
@@ -20,6 +20,18 @@ from idaes.core.util.scaling import (
     get_jacobian,
     jacobian_cond,
 )
+from pyomo.common.collections import ComponentSet
+from idaes.core.util.model_statistics import (
+    variables_in_activated_constraints_set,
+    deactivated_blocks_set,
+    activated_blocks_set,
+    activated_equalities_set,
+    deactivated_equalities_set,
+    activated_inequalities_set,
+    deactivated_inequalities_set,
+    activated_objectives_set,
+    deactivated_objectives_set,
+)
 
 # Import other needed IDAES types
 from pyomo.core.base.block import _BlockData
@@ -114,6 +126,7 @@ class ModelIssue(BaseModel):
     description: str = ""
     jacobian_condation: str = ""
     next_steps: List = []
+    toolbox_model_statistics: List = []
     toolbox_warning: List = []
     toolbox_caution: List = []
     objects: List[
@@ -306,6 +319,10 @@ class ModelIssues(BaseModel):
         numerical_cautions = self._tbx._collect_numerical_cautions(jac=jac, nlp=nlp)
 
         for item in self.issues:
+            # build model statistics
+            if item.type == "structural":
+                status = _collect_model_statistics(self._block)
+                item.toolbox_model_statistics = status
             # build jacobian condation number
             if not item.jacobian_condation:
                 item.jacobian_condation = f"Jacobian Condition Number: {jacobian_cond(jac=jac, scaled=False):.3E}"
@@ -323,3 +340,82 @@ class ModelIssues(BaseModel):
                 item.next_steps = numerical_next_steps
             if item.type == "numerical" and item.severity == "caution":
                 item.toolbox_caution = numerical_cautions
+
+
+# -------------------------------------------------------------------------------------------
+# Private module functions
+def _var_in_block(var, block):
+    parent = var.parent_block()
+    while parent is not None:
+        if parent is block:
+            return True
+        parent = parent.parent_block()
+    return False
+
+
+TAB = " " * 4
+
+
+def _collect_model_statistics(model):
+    vars_in_constraints = variables_in_activated_constraints_set(model)
+    fixed_vars_in_constraints = ComponentSet()
+    free_vars_in_constraints = ComponentSet()
+    free_vars_lb = ComponentSet()
+    free_vars_ub = ComponentSet()
+    free_vars_lbub = ComponentSet()
+    ext_fixed_vars_in_constraints = ComponentSet()
+    ext_free_vars_in_constraints = ComponentSet()
+    for v in vars_in_constraints:
+        if v.fixed:
+            fixed_vars_in_constraints.add(v)
+            if not _var_in_block(v, model):
+                ext_fixed_vars_in_constraints.add(v)
+        else:
+            free_vars_in_constraints.add(v)
+            if not _var_in_block(v, model):
+                ext_free_vars_in_constraints.add(v)
+            if v.lb is not None:
+                if v.ub is not None:
+                    free_vars_lbub.add(v)
+                else:
+                    free_vars_lb.add(v)
+            elif v.ub is not None:
+                free_vars_ub.add(v)
+
+    # Generate report
+    # TODO: Binary and boolean vars
+    stats = []
+    stats.append(
+        f"{TAB}Activated Blocks: {len(activated_blocks_set(model))} "
+        f"(Deactivated: {len(deactivated_blocks_set(model))})"
+    )
+    stats.append(
+        f"{TAB}Free Variables in Activated Constraints: "
+        f"{len(free_vars_in_constraints)} "
+        f"(External: {len(ext_free_vars_in_constraints)})"
+    )
+    stats.append(f"{TAB * 2}Free Variables with only lower bounds: {len(free_vars_lb)}")
+    stats.append(f"{TAB * 2}Free Variables with only upper bounds: {len(free_vars_ub)}")
+    stats.append(
+        f"{TAB * 2}Free Variables with upper and lower bounds: "
+        f"{len(free_vars_lbub)}"
+    )
+    stats.append(
+        f"{TAB}Fixed Variables in Activated Constraints: "
+        f"{len(fixed_vars_in_constraints)} "
+        f"(External: {len(ext_fixed_vars_in_constraints)})"
+    )
+    stats.append(
+        f"{TAB}Activated Equality Constraints: {len(activated_equalities_set(model))} "
+        f"(Deactivated: {len(deactivated_equalities_set(model))})"
+    )
+    stats.append(
+        f"{TAB}Activated Inequality Constraints: {len(activated_inequalities_set(model))} "
+        f"(Deactivated: {len(deactivated_inequalities_set(model))})"
+    )
+    stats.append(
+        f"{TAB}Activated Objectives: {len(activated_objectives_set(model))} "
+        f"(Deactivated: {len(deactivated_objectives_set(model))})"
+    )
+
+    return stats
