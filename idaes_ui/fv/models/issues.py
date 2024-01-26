16cf57e81bd2a2360f29927f186c5527a7efa9fd
CopyDemon <shengdevelop@gmail.com>
2024-01-25 16:49:25 -0800

    update diagnostics data export, move all diagnoststool box related out of Model issues class, create a diagnosticToolBoxReport class to handle all report from diagnosticToolBox from idaes core

--- idaes_ui/fv/models/issues.py
+++ idaes_ui/fv/models/issues.py
@@ -16,22 +16,6 @@ from pydantic import BaseModel
 # Import IDAES functionality (use shortened names, i=IDAES m=model)
 from idaes.core.util import model_statistics as ims
 from idaes.core.util import model_diagnostics as imd
-from idaes.core.util.scaling import (
-    get_jacobian,
-    jacobian_cond,
-)
-from pyomo.common.collections import ComponentSet
-from idaes.core.util.model_statistics import (
-    variables_in_activated_constraints_set,
-    deactivated_blocks_set,
-    activated_blocks_set,
-    activated_equalities_set,
-    deactivated_equalities_set,
-    activated_inequalities_set,
-    deactivated_inequalities_set,
-    activated_objectives_set,
-    deactivated_objectives_set,
-)
 
 # Import other needed IDAES types
 from pyomo.core.base.block import _BlockData
@@ -124,11 +108,6 @@ class ModelIssue(BaseModel):
     modifiers: Dict[str, str] = {}  # additional semi-structured info
     name: str = ""
     description: str = ""
-    jacobian_condation: str = ""
-    next_steps: List = []
-    toolbox_model_statistics: List = []
-    toolbox_warning: List = []
-    toolbox_caution: List = []
     objects: List[
         Union[
             ModelIssueVariable,
@@ -158,7 +137,6 @@ class ModelIssues(BaseModel):
         self._add_extreme_values()
         self._add_variables_near_bounds()
         self._add_not_in_act_constr()
-        self._add_next_steps()
 
     def _add_extreme_values(self):
         c = self._config
@@ -295,127 +273,3 @@ class ModelIssues(BaseModel):
             obj = ModelIssueVariable(name=v.name, value=v.value, fixed=v.fixed)
             issue.objects.append(obj)
         self.issues.append(issue)
-
-    def _add_next_steps(self):
-        """
-        use DiagnosticsToolbox functions to get next steps
-        also get warnings and cautions to put in: (as a backup output for UI)
-            issues[i].toolbox_warning
-            issues[i].toolbox_caution
-        """
-        # read structural warning cautions next stpes from toolbox
-        (
-            structural_warnings,
-            structural_next_steps,
-        ) = self._tbx._collect_structural_warnings()
-        structural_cautions = self._tbx._collect_structural_cautions()
-
-        # read numerical warning cautions next stpes from toolbox
-        jac, nlp = get_jacobian(self._block, scaled=False)
-        (
-            numerical_warnings,
-            numerical_next_steps,
-        ) = self._tbx._collect_numerical_warnings(jac=jac, nlp=nlp)
-        numerical_cautions = self._tbx._collect_numerical_cautions(jac=jac, nlp=nlp)
-
-        for item in self.issues:
-            # build model statistics
-            if item.type == "structural":
-                status = _collect_model_statistics(self._block)
-                item.toolbox_model_statistics = status
-            # build jacobian condation number
-            if not item.jacobian_condation:
-                item.jacobian_condation = f"Jacobian Condition Number: {jacobian_cond(jac=jac, scaled=False):.3E}"
-
-            # structural
-            if item.type == "structural" and item.severity == "warning":
-                item.toolbox_warning = structural_warnings
-                item.next_steps = structural_next_steps
-            if item.type == "structural" and item.severity == "caution":
-                item.toolbox_caution = structural_cautions
-
-            # numerical
-            if item.type == "numerical" and item.severity == "warning":
-                item.toolbox_warning = numerical_warnings
-                item.next_steps = numerical_next_steps
-            if item.type == "numerical" and item.severity == "caution":
-                item.toolbox_caution = numerical_cautions
-
-
-# -------------------------------------------------------------------------------------------
-# Private module functions
-def _var_in_block(var, block):
-    parent = var.parent_block()
-    while parent is not None:
-        if parent is block:
-            return True
-        parent = parent.parent_block()
-    return False
-
-
-TAB = " " * 4
-
-
-def _collect_model_statistics(model):
-    vars_in_constraints = variables_in_activated_constraints_set(model)
-    fixed_vars_in_constraints = ComponentSet()
-    free_vars_in_constraints = ComponentSet()
-    free_vars_lb = ComponentSet()
-    free_vars_ub = ComponentSet()
-    free_vars_lbub = ComponentSet()
-    ext_fixed_vars_in_constraints = ComponentSet()
-    ext_free_vars_in_constraints = ComponentSet()
-    for v in vars_in_constraints:
-        if v.fixed:
-            fixed_vars_in_constraints.add(v)
-            if not _var_in_block(v, model):
-                ext_fixed_vars_in_constraints.add(v)
-        else:
-            free_vars_in_constraints.add(v)
-            if not _var_in_block(v, model):
-                ext_free_vars_in_constraints.add(v)
-            if v.lb is not None:
-                if v.ub is not None:
-                    free_vars_lbub.add(v)
-                else:
-                    free_vars_lb.add(v)
-            elif v.ub is not None:
-                free_vars_ub.add(v)
-
-    # Generate report
-    # TODO: Binary and boolean vars
-    stats = []
-    stats.append(
-        f"{TAB}Activated Blocks: {len(activated_blocks_set(model))} "
-        f"(Deactivated: {len(deactivated_blocks_set(model))})"
-    )
-    stats.append(
-        f"{TAB}Free Variables in Activated Constraints: "
-        f"{len(free_vars_in_constraints)} "
-        f"(External: {len(ext_free_vars_in_constraints)})"
-    )
-    stats.append(f"{TAB * 2}Free Variables with only lower bounds: {len(free_vars_lb)}")
-    stats.append(f"{TAB * 2}Free Variables with only upper bounds: {len(free_vars_ub)}")
-    stats.append(
-        f"{TAB * 2}Free Variables with upper and lower bounds: "
-        f"{len(free_vars_lbub)}"
-    )
-    stats.append(
-        f"{TAB}Fixed Variables in Activated Constraints: "
-        f"{len(fixed_vars_in_constraints)} "
-        f"(External: {len(ext_fixed_vars_in_constraints)})"
-    )
-    stats.append(
-        f"{TAB}Activated Equality Constraints: {len(activated_equalities_set(model))} "
-        f"(Deactivated: {len(deactivated_equalities_set(model))})"
-    )
-    stats.append(
-        f"{TAB}Activated Inequality Constraints: {len(activated_inequalities_set(model))} "
-        f"(Deactivated: {len(deactivated_inequalities_set(model))})"
-    )
-    stats.append(
-        f"{TAB}Activated Objectives: {len(activated_objectives_set(model))} "
-        f"(Deactivated: {len(deactivated_objectives_set(model))})"
-    )
-
-    return stats
