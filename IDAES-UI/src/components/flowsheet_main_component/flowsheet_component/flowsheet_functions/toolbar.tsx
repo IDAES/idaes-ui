9160afa24bfa927ec15fbb81851e42126f688161
CopyDemon <shengdevelop@gmail.com>
2024-02-14 16:59:29 -0800

    fix clean up function call location, instead of call in zoom btn click now call in mainFV constractor last step

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_component/flowsheet_functions/toolbar.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_component/flowsheet_functions/toolbar.tsx
@@ -120,44 +120,16 @@ export class Toolbar {
    * create zoom events handler
    */
   zoomInEvent = () =>{
-        this.zoomClearExtraFvElement() // this clear extra fv display stack on top of newest one
         this._paper.paperScroller.zoom(0.2, { max: 100 });
   }
 
   zoomOutEvent = () =>{
-    this.zoomClearExtraFvElement() // this clear extra fv display stack on top of newest one
     this._paper.paperScroller.zoom(-0.2, { min: 0 });
   }
 
   zoomFitEvent = () =>{
     this._paper.zoomToFit();
         console.log(`working zoom fit`)
-  }
-
-  /**
-   * @Description This function help check if fv has mutiple children,
-   * if has will remove all children and keep the last one.
-   * 
-   * @Reason When react render, will create a new instence of MainFv,
-   * it will create a new fv display stack under the old one.
-   * this behivor cause zoom in and out btn not working so we need to clear all
-   * fv display other than the last one.
-   * 
-   * @returns void
-   */
-  zoomClearExtraFvElement = () =>{
-    let fv = document.getElementById("fv");
-
-    //validation if fv and fv has mutiple child
-    if(!fv || fv.childNodes.length <= 1){
-        return;
-    }
-    
-    //get fv last child and remove others
-    let lastFvChild = fv.childNodes[fv?.childNodes.length - 1]
-    while(fv.firstChild !== fv.lastChild){
-        fv.removeChild(fv.firstChild as Node)
-    }
   }
 
   /**
