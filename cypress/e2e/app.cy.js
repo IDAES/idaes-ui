972d9e9e8e06860f2c48f1f61fe99f60e1ea2f02
Dan Gunter <dkgunter@lbl.gov>
2024-03-22 06:58:36 -0700

    reverted

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,34 +1,36 @@
-describe(
-    'loads the app UI correctly',
-    {
-        retries: {
-            runMode: 2,
-            openMode: 2
-        }
-    },
-    () => {
-        // base on webapp url "baseUrl" visit webapp
-        beforeEach(() => {
-            cy.visit(Cypress.config('baseUrl'));
-        })
+describe('loads the app UI correctly', () => {
+  // base on webapp url "baseUrl" visit webapp
+  beforeEach(() =>{
+    cy.visit(Cypress.config('baseUrl'));
+  })
+  
+  //check if app component exists
+  it('check app #root exists', () => {
+    cy.get('#root').should('exist');
+    cy.log(`App is up and running at ${Cypress.config('baseUrl')}`);
+  })
 
-        //check if app component exists
-        it('check app #root exists', () => {
-            cy.get('#root').should('exist');
-        })
+  //check if header component exists
+  it('check app #header exists', () => {
+    cy.get('#header').should('exist');
+  })
 
-        // check if header component exists
-        it('check app #header exists', () => {
-            cy.get('#header').should('exist');
-        })
+  //check flowsheet component exists
+  it('check app #flowsheet-wrapper exists', () => {
+    cy.get('#flowsheet-wrapper').should('exist');
+  })
 
-        // check flowsheet component exists
-        it('check app #flowsheet-wrapper exists', () => {
-            cy.get('#flowsheet-wrapper').should('exist');
-        })
+  //check stream table component exists
+  it('check app #stream-table exist', () => {
+    cy.get('#stream-table').should('exist');
+  })
 
-        // check stream table component exists
-        it('check app #stream-table exist', () => {
-            cy.get('#stream-table').should('exist');
-        })
-    })
+
+  /**
+   * This comment out on purpose and if enabled, will use it to test if CI can fail
+   */
+  //loading fake test check if test working
+  // it('loads stream table', () => {
+  //   cy.get('#unexist-id').should('exist');
+  // })
+})
