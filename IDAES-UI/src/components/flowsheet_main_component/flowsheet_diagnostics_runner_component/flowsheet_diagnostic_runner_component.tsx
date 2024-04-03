2eed3d1e319ab827229c7a0e1638296f8d9cce97
CopyDemon <shengdevelop@gmail.com>
2024-04-02 22:20:43 -0700

    update diagnostics runner container over flow to style sheet, add scroll smooth

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_runner_component/flowsheet_diagnostic_runner_component.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_runner_component/flowsheet_diagnostic_runner_component.tsx
@@ -1,30 +1,39 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "@/context/appMainContext";
 import css from "./flowsheet_diagnostics_runner.module.css";
 export default function FlowsheetDiagnosticsRunner(){
-    const {diagnosticsNextStepsOutputState, diagnosticsRunnerDisplayState} = useContext(AppContext);
+    const {
+            diagnosticsNextStepsOutputState, 
+            diagnosticsRunnerDisplayState,
+            diagnosticsHistoryState,
+            setDiagnosticsHistory
+        } = useContext(AppContext);
     
     // initial run function display
     let display:any = "";
+    let displayLength:number = 0
 
     if(!diagnosticsRunnerDisplayState ||  diagnosticsRunnerDisplayState == "default"){
         display = "Please select a function to check diagnostics result!"
     }else if(!diagnosticsNextStepsOutputState[diagnosticsRunnerDisplayState] || diagnosticsNextStepsOutputState[diagnosticsRunnerDisplayState][0].diagnostics_runner_result.length == 0){
-        console.log(`in`)
         display = "Please run diagnostics function first!"
     }else if(diagnosticsNextStepsOutputState[diagnosticsRunnerDisplayState] && diagnosticsNextStepsOutputState[diagnosticsRunnerDisplayState]){
-        console.log()
+        // get current diagnostics's length
+        displayLength = diagnosticsNextStepsOutputState[diagnosticsRunnerDisplayState].length;
+        
         display = diagnosticsNextStepsOutputState[diagnosticsRunnerDisplayState].map((el:any, index:number)=>{
             return(
                 <React.Fragment key={`diagnosticsRunnerDisplayContentContainerContent${index}`}>
-                    <pre className={css.diagnostics_runner_output_pre}>{el.diagnostics_runner_result}</pre>
+                    <pre id={`diagnostics_log_${index}`}className={css.diagnostics_runner_output_pre}>{el.diagnostics_runner_result}</pre>
                 </React.Fragment>
             )
         })
     }
 
-    console.log()
+    useEffect(()=>{
+        setDiagnosticsHistory((prev:number)=>displayLength)
+    },[displayLength])
 
     return(
         <div className={css.diagnosticsRunner_content_container} style={{"overflowY" : "scroll"}}>
