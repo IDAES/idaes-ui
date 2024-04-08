6af4806664537c240766cb7199ae9c73c8f65ae6
CopyDemon <shengdevelop@gmail.com>
2024-04-08 15:54:28 -0700

    resolve conflict remove fastapi related

--- idaes_ui/fv/app.py
+++ idaes_ui/fv/app.py
@@ -6,14 +6,7 @@ __author__ = "Dan Gunter"
 __created__ = "2023-10-08"
 
 # stdlib
-<<<<<<< HEAD
 from pathlib import Path
-=======
-import sys
-from typing import Optional
-
-# from pathlib import Path
->>>>>>> 219848c (add save dir and define self.save dir)
 
 # external packages
 from fastapi import FastAPI, HTTPException
@@ -30,44 +23,15 @@ class FlowsheetApp:
     _root_dir = Path(__file__).parent.absolute()  # static dir in same dir as this file
     _static_dir = _root_dir / "static"
 
-<<<<<<< HEAD
     def __init__(self, flowsheet, name="my flowsheet"):
         self.app = FastAPI()
         self.app.mount(
             "/static", StaticFiles(directory=self._static_dir), name="static"
-=======
-    def __init__(
-        self, flowsheet, name, port, save_time_interval, save_dir: Optional[str] = None
-    ):
-        # populate web port
-        if port:
-            self.port = port
-        else:
-            self.port = 8000
-
-        # initial save dir
-        if save_dir:
-            self.save_dir = save_dir
-        else:
-            self.save_dir = "./saved_flowsheet"
-
-        # initial everything related to flowsheet
-        self.flowsheet = flowsheet
-        self.flowsheet_name = name
-
-        # initial FastAPI
-        self.app = FastAPI(
-            docs_url="/api/v1/docs",
-            redoc="/api/v1/redoc",
-            title="IDAES UI API DOC",
-            description="IDAES UI API endpoint detail.",
->>>>>>> 219848c (add save dir and define self.save dir)
         )
         self.diag_data = DiagnosticsData(flowsheet)
         self.settings = AppSettings()
         self.flowsheet = Flowsheet(fs=flowsheet, name=name)
 
-<<<<<<< HEAD
         @self.app.get("/diagnostics/")
         async def get_diagnostics() -> DiagnosticsData:
             try:
@@ -79,20 +43,6 @@ class FlowsheetApp:
         @self.app.get("/settings/")
         def get_settings() -> AppSettings:
             return self.settings
-=======
-        # # API router
-<<<<<<< HEAD
-        Router(self.app, self.flowsheet, self.flowsheet_name, self.save_time_interval)
->>>>>>> 9f11168 (remove get flowsheet it's already in it's own route file)
-=======
-        Router(
-            fastAPIApp=self.app,
-            flowsheet=self.flowsheet,
-            flowsheet_name=self.flowsheet_name,
-            save_dir=self.save_dir,
-            save_time_interval=self.save_time_interval,
-        )
->>>>>>> 9cc24b1 (add flowsheet store location save_dir)
 
         @self.app.put("/settings/")
         def put_settings(settings: AppSettings):
