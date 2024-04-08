13da025de3af360987fb543b6102d3e1ad5d9388
CopyDemon <shengdevelop@gmail.com>
2024-04-08 15:52:46 -0700

    rebase resolve conflict base on current

--- idaes_ui/fv/app.py
+++ idaes_ui/fv/app.py
@@ -6,7 +6,14 @@ __author__ = "Dan Gunter"
 __created__ = "2023-10-08"
 
 # stdlib
+<<<<<<< HEAD
 from pathlib import Path
+=======
+import sys
+from typing import Optional
+
+# from pathlib import Path
+>>>>>>> 219848c (add save dir and define self.save dir)
 
 # external packages
 from fastapi import FastAPI, HTTPException
@@ -23,10 +30,37 @@ class FlowsheetApp:
     _root_dir = Path(__file__).parent.absolute()  # static dir in same dir as this file
     _static_dir = _root_dir / "static"
 
+<<<<<<< HEAD
     def __init__(self, flowsheet, name="my flowsheet"):
         self.app = FastAPI()
         self.app.mount(
             "/static", StaticFiles(directory=self._static_dir), name="static"
+=======
+    def __init__(
+        self, flowsheet, name, port, save_time_interval, save_dir: Optional[str] = None
+    ):
+        # populate web port
+        if port:
+            self.port = port
+        else:
+            self.port = 8000
+        # initial save dir
+        if save_dir:
+            self.save_dir = save_dir
+        else:
+            self.save_dir = "./saved_flowsheet"
+
+        # initial everything related to flowsheet
+        self.flowsheet = flowsheet
+        self.flowsheet_name = name
+
+        # initial FastAPI
+        self.app = FastAPI(
+            docs_url="/api/v1/docs",
+            redoc="/api/v1/redoc",
+            title="IDAES UI API DOC",
+            description="IDAES UI API endpoint detail.",
+>>>>>>> 219848c (add save dir and define self.save dir)
         )
         self.diag_data = DiagnosticsData(flowsheet)
         self.settings = AppSettings()
