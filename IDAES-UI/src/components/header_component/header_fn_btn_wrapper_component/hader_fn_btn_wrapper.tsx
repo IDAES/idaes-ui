259f83f3bdd852c8f019768e0ed5a1519a02acf6
CopyDemon <shengdevelop@gmail.com>
2024-04-23 09:52:08 -0700

    add layout reset function btn at header

--- IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/hader_fn_btn_wrapper.tsx
+++ IDAES-UI/src/components/header_component/header_fn_btn_wrapper_component/hader_fn_btn_wrapper.tsx
@@ -1,6 +1,7 @@
 import HeaderFNBtnRefresh from "./header_fn_btn_refresh_component/header_fn_btn_refresh";
 import HeaderFNBtnSave from "./header_fn_btn_save_component/header_fn_btn_save";
 import HeaderFNBtnHelp from "./header_fn_btn_help_component/header_fn_btn_help_component";
+import HeaderFNBtnLayoutReset from "./header_fn_btn_layout_reset_component/header_fn_btn_layout_reset_component";
 
 import css from "./header_fn_btn.module.css"
 export default function HeaderFunctionButtonsWrapper(){
@@ -8,6 +9,7 @@ export default function HeaderFunctionButtonsWrapper(){
     <ul className={`${css.header_fn_btn_wrapper_ul}`}>
       <HeaderFNBtnRefresh />
       <HeaderFNBtnSave />
+      <HeaderFNBtnLayoutReset />
       <HeaderFNBtnHelp />
     </ul>
   )
