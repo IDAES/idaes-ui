3be7cb1836546d0f2ff7a64e66d06d519b8b9531
CopyDemon <shengdevelop@gmail.com>
2024-09-09 23:12:09 -0700

    update browser use no sandbox mode

--- idaes_ui/fv/fsvis.py
+++ idaes_ui/fv/fsvis.py
@@ -416,7 +416,9 @@ async def _async_save_diagram(
     from IPython.display import display as IPythonDisplay
 
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, timeout=300000)
+        browser = await p.chromium.launch(
+            headless=True, args=["--no-sandbox"], timeout=300000
+        )
         context = await browser.new_context(viewport={"width": 1920, "height": 1080})
         page = await context.new_page()
 
