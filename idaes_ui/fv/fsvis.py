30c21576a0e523f2312bedd9b0887c1b76f2c7b9
CopyDemon <shengdevelop@gmail.com>
2024-10-22 10:17:52 -0700

    increase timeout time

--- idaes_ui/fv/fsvis.py
+++ idaes_ui/fv/fsvis.py
@@ -417,7 +417,7 @@ async def _async_save_diagram(
 
     async with async_playwright() as p:
         browser = await p.chromium.launch(
-            headless=True, args=["--no-sandbox"], timeout=300000
+            headless=True, args=["--no-sandbox"], timeout=800000
         )
         context = await browser.new_context(viewport={"width": 1920, "height": 1080})
         page = await context.new_page()
